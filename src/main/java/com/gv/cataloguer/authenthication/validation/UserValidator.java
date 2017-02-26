42c55c80d58bbca96a6936e0998a690873366ab4
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-02-26 21:53:02 +0300

    added class UserValidator

---
+++ src/main/java/com/gv/cataloguer/authenthication/validation/UserValidator.java
@@ -0,0 +1,23 @@
+package com.gv.cataloguer.authenthication.validation;
+
+import com.gv.cataloguer.authenthication.dao.UserDao;
+import com.gv.cataloguer.authenthication.dao.UserDaoSingleton;
+import com.gv.cataloguer.models.User;
+
+import java.sql.SQLException;
+
+public class UserValidator {
+
+    public static User checkLogin(String login, String password) {
+        User user = null;
+        try {
+            user = UserDaoSingleton.getInstance().getUser(login, password);
+        } catch (SQLException e){
+            e.printStackTrace();
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } finally {
+            return user;
+        }
+    }
+}
