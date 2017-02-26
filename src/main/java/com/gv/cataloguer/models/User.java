02868aefe343aaba4f5f68f45c6c5e52396a4ff9
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-02-26 21:39:53 +0300

    added class User and enum Role

---
+++ src/main/java/com/gv/cataloguer/models/User.java
@@ -0,0 +1,35 @@
+package com.gv.cataloguer.models;
+
+public class User {
+    private int userId;
+    private String login;
+    private String name;
+    private Role role;
+
+    public User(Role role) {
+        this.role = role;
+    }
+
+    public User(int userId, String login, String name, Role role) {
+        this.userId = userId;
+        this.login = login;
+        this.name = name;
+        this.role = role;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+}
