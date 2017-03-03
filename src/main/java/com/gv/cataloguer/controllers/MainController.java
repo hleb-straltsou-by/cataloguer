a26bdaad4376db1b0228af408608fa69a4e845a4
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-03-03 20:42:46 +0300

    updated controllers

--- src/main/java/com/gv/cataloguer/controllers/MainController.java
+++ src/main/java/com/gv/cataloguer/controllers/MainController.java
@@ -1,8 +1,17 @@
 package com.gv.cataloguer.controllers;
 
+import com.gv.cataloguer.gui.start.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainController {
     @FXML
@@ -18,8 +27,30 @@ public class MainController {
     private Label suggestLabel;
 
     @FXML
+    private TableView tableView;
+
+    @FXML
+    private Label logInLabel;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private TreeView<String> treeView;
+
+    private static final FileChooser fileChooser = new FileChooser();
+
+    @FXML
     private void initialize(){
-        switch (FormController.currentRole){
+        logInLabel.setText("You are entered as:\n" + FormController.currentUser.getName());
+        treeView.setRoot(new TreeItem<String>("Catalog"));
+        TreeItem rootItem = treeView.getRoot();
+        rootItem.setExpanded(true);
+        rootItem.getChildren().add(new TreeItem<String>("Music"));
+        rootItem.getChildren().add(new TreeItem<String>("Movies"));
+        rootItem.getChildren().add(new TreeItem<String>("Books"));
+        rootItem.getChildren().add(new TreeItem<String>("Documents"));
+        switch (FormController.currentUser.getRole()){
             case ADMIN:
                 suggestButton.setVisible(false);
                 suggestLabel.setVisible(false);
@@ -33,6 +64,31 @@ public class MainController {
                 suggestButton.setVisible(false);
                 suggestLabel.setVisible(false);
                 break;
+        }
+    }
+
+    public void logout(ActionEvent actionEvent) {
+        try {
+            Stage stage = Main.getMainStage();
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/form.fxml"));
+            stage.setMinWidth(Main.MIN_WIDTH_OF_FORM_WINDOW);
+            stage.setMinHeight(Main.MIN_HEIGHT_OF_FORM_WINDOW);
+            stage.setMaxWidth(Main.MIN_WIDTH_OF_FORM_WINDOW);
+            stage.setMaxHeight(Main.MIN_HEIGHT_OF_FORM_WINDOW);
+            stage.setResizable(false);
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void addNewFile(ActionEvent actionEvent) throws IOException {
+        fileChooser.setTitle("Open Resource File");
+        File file = fileChooser.showOpenDialog(Main.getMainStage());
+        if (file != null) {
+            treeView.getRoot().getChildren().add(new TreeItem<String>(file.getName()));
         }
     }
 }
