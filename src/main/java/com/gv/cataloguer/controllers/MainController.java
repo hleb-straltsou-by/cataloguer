04575f71af1f0e508fc3a701ad3d5084c6f82a9f
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-03-04 23:10:26 +0300

    updated controllers

--- src/main/java/com/gv/cataloguer/controllers/MainController.java
+++ src/main/java/com/gv/cataloguer/controllers/MainController.java
@@ -1,19 +1,25 @@
 package com.gv.cataloguer.controllers;
 
-import com.gv.cataloguer.gui.start.Main;
+import com.gv.cataloguer.catalog.ResourceCatalog;
+import com.gv.cataloguer.start.Main;
+import com.gv.cataloguer.models.Reference;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class MainController {
+
     @FXML
     private Button addButton;
 
@@ -27,7 +33,7 @@ public class MainController {
     private Label suggestLabel;
 
     @FXML
-    private TableView tableView;
+    private ListView<String> categories;
 
     @FXML
     private Label logInLabel;
@@ -36,20 +42,22 @@ public class MainController {
     private Button logoutButton;
 
     @FXML
-    private TreeView<String> treeView;
+    private TreeTableView<Reference> treeTableView;
+
+    @FXML
+    private TreeTableColumn<Reference, String> nameColumn;
+
+    @FXML
+    private TreeTableColumn<Reference, Long> sizeColumn;
+
+    @FXML
+    private TreeTableColumn<Reference, String> lastModifiedColumn;
 
     private static final FileChooser fileChooser = new FileChooser();
 
     @FXML
     private void initialize(){
         logInLabel.setText("You are entered as:\n" + FormController.currentUser.getName());
-        treeView.setRoot(new TreeItem<String>("Catalog"));
-        TreeItem rootItem = treeView.getRoot();
-        rootItem.setExpanded(true);
-        rootItem.getChildren().add(new TreeItem<String>("Music"));
-        rootItem.getChildren().add(new TreeItem<String>("Movies"));
-        rootItem.getChildren().add(new TreeItem<String>("Books"));
-        rootItem.getChildren().add(new TreeItem<String>("Documents"));
         switch (FormController.currentUser.getRole()){
             case ADMIN:
                 suggestButton.setVisible(false);
@@ -65,6 +73,9 @@ public class MainController {
                 suggestLabel.setVisible(false);
                 break;
         }
+        nameColumn.setCellValueFactory(new TreeItemPropertyValueFactory("name"));
+        sizeColumn.setCellValueFactory(new TreeItemPropertyValueFactory("size"));
+        lastModifiedColumn.setCellValueFactory(new TreeItemPropertyValueFactory("lastModified"));
     }
 
     public void logout(ActionEvent actionEvent) {
@@ -88,7 +99,23 @@ public class MainController {
         fileChooser.setTitle("Open Resource File");
         File file = fileChooser.showOpenDialog(Main.getMainStage());
         if (file != null) {
-            treeView.getRoot().getChildren().add(new TreeItem<String>(file.getName()));
+            String category = categories.getSelectionModel().getSelectedItem();
+            ResourceCatalog.getInstance().addResourceToCatalog(category, file);
         }
+    }
+
+    public void selectCategory(MouseEvent mouseEvent) {
+        String category = categories.getSelectionModel().getSelectedItem();
+        treeTableView.setRoot(new TreeItem<>(new Reference(category)));
+        TreeItem rootItem = treeTableView.getRoot();
+        rootItem.setExpanded(true);
+        List<Reference> references = ResourceCatalog.getInstance().getCategory(category);
+        for(Reference ref : references){
+            rootItem.getChildren().add(new TreeItem<>(ref));
+        }
+//        rootItem.getChildren().add(new TreeItem<String>("Music"));
+//        rootItem.getChildren().add(new TreeItem<String>("Movies"));
+//        rootItem.getChildren().add(new TreeItem<String>("Books"));
+//        rootItem.getChildren().add(new TreeItem<String>("Documents"));
     }
 }
