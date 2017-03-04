8dff91c5f8862d5af8443b2e22c9c6775f9fb48b
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-03-04 23:18:41 +0300

    updated interface UserDao and class UserDaoSingleton

--- src/main/java/com/gv/cataloguer/authenthication/dao/UserDao.java
+++ src/main/java/com/gv/cataloguer/authenthication/dao/UserDao.java
@@ -1,10 +1,9 @@
 package com.gv.cataloguer.authenthication.dao;
 
 import com.gv.cataloguer.models.User;
-
 import java.sql.SQLException;
 
 public interface UserDao {
 
-    public User getUser(String login, String password) throws SQLException, ClassNotFoundException;
+    User getUser(String login, String password) throws SQLException, ClassNotFoundException;
 }
